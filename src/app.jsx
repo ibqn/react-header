d0284efc3cc17775aa7a8f1e675c7905af48cd34
ibqn <evgen.ibqn@gmail.com>
2021-05-13 11:16:31 +0200

    improve formatting

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,3 @@
-import React from "react"
-// import logo from "./logo.svg"
-// import "./App.css"
 import Header from "./components/header"
 import Body from "./components/body"
 import styled from "styled-components"
--- src/index.jsx
+++ src/index.jsx
@@ -1,19 +1,21 @@
 import React from "react"
 import ReactDOM from "react-dom"
-// import "./index.css"
 import App from "./app"
 import * as serviceWorker from "./serviceWorker"
 import { createGlobalStyle } from "styled-components"
 
-const GlobalStyle = createGlobalStyle`
+const styled = { createGlobalStyle }
+
+const GlobalStyle = styled.createGlobalStyle`
   html {
     height: 100%;
   }
 
   body {
     margin: 0;
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
-      'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto",
+      "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans",
+      "Helvetica Neue", sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     height: 100%;
